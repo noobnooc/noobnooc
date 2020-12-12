57b39fa400c235d4cdb91e6a8f220485328ce164
Nooc <nooc@nooc.ink>
2020-12-12 15:16:07 +0800

    Remove template code generated by gatsby

---
+++ src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from "typography"
+import fairyGateTheme from "typography-theme-fairy-gates"
+
+const typography = new Typography(fairyGateTheme)
+
+export const { scale, rhythm, options } = typography
+export default typography
