5fc6ca25585f004e61dad5e392de2a43ee0571f0
Nooc <nooc@nooc.ink>
2020-12-13 23:55:57 +0800

    Add animated avatar

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,21 @@
+import Lottie from "lottie-web";
 import * as React from "react";
 
 import styles from "./home.module.css";
 
 const HomePage = () => {
+  React.useEffect(() => {
+    Lottie.loadAnimation({
+      container: document.querySelector("#animated-avatar-container"),
+      autoplay: true,
+      loop: false,
+      path: "/lottie/animated-avatar.json",
+    });
+  });
+
   return (
     <div className={styles.wrapper}>
-      <img
-        className={styles.avatar}
-        alt="Nooc"
-        src="/images/circle-avatar.svg"
-      />
+      <div id="animated-avatar-container" className={styles.avatar} />
       <p>constructing ...</p>
     </div>
   );
